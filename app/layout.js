87d9a77cb964cf9e8c25445a8f9fc61f5f09c212
zubair7384 <zubair.iqbal2018@gmail.com>
2023-05-09 21:59:16 +0400

    new

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-"use client";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
@@ -6,10 +5,10 @@ import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "Rameez Iqbal rodtfolio",
-//   description: "Rameez Iqbal portfolio description",
-// };
+export const metadata = {
+  title: "Rameez Iqbal rodtfolio",
+  description: "Rameez Iqbal portfolio description",
+};
 
 export default function RootLayout({ children }) {
   return (
